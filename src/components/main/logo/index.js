1fc8bd0cc61a4ab3bdc8f942ddb9981901a398df
igorshevk <igor.shevk@gmail.com>
2020-10-06 09:03:53 -0700

    update issue

--- src/components/main/logo/index.js
+++ src/components/main/logo/index.js
@@ -15,7 +15,7 @@ const Logo = () => {
 	React.useEffect(()=>{
 		dispatch(getDynamicAds())
 	},[])
-	const dynamicAds = useSelector(state=>state.utils.ads[0]["banner"])
+	const dynamicAds = useSelector(state=>state.utils.ads)
     return (
         	<SliderWrapper>
         	<Slider
@@ -24,7 +24,7 @@ const Logo = () => {
 	          adaptiveHeight={true}
 	          autoplay
 	        >
-	          {dynamicAds && dynamicAds.map((item, index) => {
+	          {dynamicAds[0] && dynamicAds[0]["banner"] && dynamicAds[0]["banner"].map((item, index) => {
 	            return <DynamicContent key={index}><AdsImg src={item.image}></AdsImg></DynamicContent>;
 	          })}
 	        </Slider>
