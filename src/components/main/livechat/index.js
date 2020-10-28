385b43c855604656dd679d3e71ee65d12383e3b0
Ali Abbas Malik <aliabbasmalik8@gmail.com>
2020-10-28 14:02:44 -0700

    added one to one chat

--- src/components/main/livechat/index.js
+++ src/components/main/livechat/index.js
@@ -1,33 +1,89 @@
-import React, { useEffect } from 'react';
-import { Widget, addResponseMessage } from 'react-chat-widget';
+import React, { useEffect } from "react";
+import { Widget, addResponseMessage, dropMessages } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import io from "socket.io-client";
 
-import 'react-chat-widget/lib/styles.css';
+import logo from "./logo192.png";
+import { Livechat } from "./styles";
+import { useState } from "react";
 
-import logo from './logo192.png';
-import { 
-	Livechat
-} from "./styles"
+const socket = io.connect("https://immense-river-10142.herokuapp.com/");
+socket.on("message", (data) => addResponseMessage(data.message));
 
 function LiveChat() {
+  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  socket.on("list.room", (users) => {
+    setUsers(users);
+  });
+
+  socket.on("join", (room) => {
+    dropMessages();
+    setSelectedRoom(room);
+  });
+
   useEffect(() => {
-    addResponseMessage('Welcome to this awesome chat!');
+    addResponseMessage("Welcome to this awesome chat!");
   }, []);
 
   const handleNewUserMessage = (newMessage) => {
-    console.log(`New message incoming! ${newMessage}`);
-    // Now send the message throught the backend API
+    socket.emit("message", { room: selectedRoom, message: newMessage });
   };
 
-    return (
-     <Livechat>
+  const joinRoom = (room) => {
+    socket.emit("join", room);
+  };
+
+  return (
+    <Livechat>
+      {selectedRoom && (
         <Widget
           handleNewUserMessage={handleNewUserMessage}
-         profileAvatar={logo}
+          profileAvatar={logo}
           title="React eCom site"
           subtitle="And my cool subtitle"
         />
-      </Livechat>
-    );
+      )}
+      <RoomList list={users} join={joinRoom} />
+    </Livechat>
+  );
 }
+
+const RoomList = ({ list, join }) => {
+  return (
+    <div
+      className="card p-2"
+      style={{
+        position: "fixed",
+        right: "50px",
+        bottom: "100px",
+        height: "350px",
+        width: "250px",
+        overflowY: "auto",
+      }}
+    >
+      <h5 style={{color: '#0d97ff'}}>Users Listing</h5>
+      <ul className="list-group" style={{ border: 0 }}>
+        {list &&
+          list.map((room) => (
+            <li
+              className="d-flex align-items-center px-2 py-1"
+              key={room}
+              onClick={() => join(room)}
+              style={{
+                border: 0,
+                borderBottom: "1px solid #ccc",
+                wordBreak: 'break-word'
+              }}
+            >
+              <img src="https://s3.amazonaws.com/37assets/svn/765-default-avatar.png" className="mr-2"  width="30" height="30"/>
+              {room}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default LiveChat;
